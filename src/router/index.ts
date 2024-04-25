de8079284040b331f0172ea5932897b0fcd33ac8
devUserContact <developer.user.contact@protonmail.com>
2024-04-24 21:06:40 -0700

    added `/shop` to router

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,6 +17,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
     }
+    {
+      path: '/shop',
+      name: 'shop',
+      component: () => import('../views/ShopView.vue')
+    }
   ]
 })
 
