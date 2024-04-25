191a9758394c4e82001dd3c350c70f5f1bcd27c7
devUserContact <developer.user.contact@protonmail.com>
2024-04-24 20:55:56 -0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home </RouterLink>
+        <RouterLink to="/about">About </RouterLink>
+      </nav>
+    </div>
+  </header>
+  <body>
+    <RouterView />
+  </body>
+</template>
+
+<style scoped>
+header {
+  display: grid;
+  height: 5%;
+  border: solid;
+}
+
+nav {
+  width: 100vh;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+@media (min-width: 1024px) {
+  header .wrapper {
+    display: grid;
+  }
+
+  nav {
+    text-align: left;
+    font-size: 1rem;
+    margin-top: 1rem;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <div class="home">
+      <p class="landingText">
+        mhwPaint
+      </p>
+    </div>
+  </main>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .home {
+    display: grid;
+    border: solid;
+  }
+  .landingText {
+    text-align: left;
+  }
+}
+</style>
